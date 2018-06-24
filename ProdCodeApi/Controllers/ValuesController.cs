67357d56bc0a75127b5041762589a35a89ae5bb6
Stoyan Grigorov <grigorov.stoyan@gmail.com>
2018-06-24 22:13:30 +0300

    ProdCode api basic JWT auth setup :rocket:

---
+++ ProdCodeApi/Controllers/ValuesController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProdCodeApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet, Authorize]
+        public IEnumerable<string> Get()
+        {
+            int userAge = 0;
+            var currentUser = HttpContext.User;
+
+            if (currentUser.HasClaim(c => c.Type == ClaimTypes.DateOfBirth))
+            {
+                DateTime birthDate = DateTime.Parse(currentUser.Claims.FirstOrDefault(c => c.Type == ClaimTypes.DateOfBirth).Value);
+                userAge = DateTime.Today.Year - birthDate.Year;
+            }
+
+            if (userAge < 18)
+            {
+                return new string[] { "value0", "value1", "value2" };
+            }
+            else
+            {
+                return new string[] { "value3", "value4" };
+            }
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
